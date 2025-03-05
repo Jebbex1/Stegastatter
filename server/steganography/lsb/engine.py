36a7560ffd2c70c28fc4a4f8e68d365b83fe1598
Yahel Statter <jebbex42+github@gmail.com>
2025-03-05 15:31:37 +0200

    Fixed LSB algorithm issue

--- server/steganography/lsb/engine.py
+++ server/steganography/lsb/engine.py
@@ -27,3 +27,20 @@ def lsb_check_if_fits_from_arbitrary(source_image_bytes: bytes, arbitrary_byte_l
     img = LSBImage(source_image_bytes, num_of_sacrificed_bits)
     wrapped_length = math.ceil(ecc_block_size * ((arbitrary_byte_length + 16) / (ecc_block_size - ecc_symbol_num)))
     return img.check_capacity(wrapped_length * 8)
+
+
+if __name__ == '__main__':
+    # in -> 33152
+    in_image_path = "server/steganography/lsb/big2.png"
+    out_image_path = "server/steganography/lsb/out1.png"
+    message_in_path = "server/steganography/lsb/message.txt"
+    message_out_path = "server/steganography/lsb/message_out.txt"
+    num_of_sacrificed_bits = 2
+
+    img = LSBImage(open(in_image_path, "rb").read(), num_of_sacrificed_bits)
+    new_image = img.encode(open(message_in_path, "rb").read(), True)
+    open(out_image_path, "wb").write(image_to_bytes(new_image))
+
+    stegged_image = LSBImage(open(out_image_path, "rb").read(), num_of_sacrificed_bits)
+    text_out = stegged_image.decode()
+    open(message_out_path, "wb").write(text_out)
