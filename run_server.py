13e0fe783e55cdf54b83c5cc4dd45f15eab5000c
Jebbex <107323805+Jebbex1@users.noreply.github.com>
2025-06-07 15:02:34 +0300

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -6,5 +6,7 @@ if __name__ == '__main__':
     record_secrets = False
     if len(sys.argv) > 1:
         record_secrets = sys.argv[1] == '--rec-secrets'
+    if record_secrets:
+        print("Recording TLS secrets!")
     s = Server(record_secrets)
     s.start()
