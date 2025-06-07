780cea600ffb70d68ed0b18e80af308e7bf131db
Jebbex <107323805+Jebbex1@users.noreply.github.com>
2025-06-07 13:37:48 +0300

    Split the cert generators into separate scripts

--- server_cert_generator.py
+++ server_cert_generator.py
@@ -9,33 +9,12 @@ EXTFILE = "server/certificate/extfile.cnf"
 SERVER_CERT = "server/certificate/cert.pem"
 
 
-EXTFILE_CONTENTS = (f"subjectAltName = @alt_names\n\n"
-                    f"[alt_names]\n"
-                    f"DNS.1 = stegastatter.com\n"
-                    f"IP.1 = 127.0.0.1\n"
-                    f"IP.2 = 192.168.77.2\n"
-                    f"IP.3 = 192.168.77.3\n"
-                    f"IP.4 = 192.168.77.4\n"
-                    f"IP.5 = 192.168.77.5\n")
-
-
-def generate_ca():
-    subprocess.run(["openssl", "genrsa", "-aes256", "-out", f"\'{CA_KEY}\'", "4096"],
-                   stderr=sys.stdout)
-    subprocess.run(["openssl", "req", "-new", "-x509", "-sha256", "-days", "3650", "-key", f"\'{CA_KEY}\'", "-out",
-                    f"\'{CA_CERT}\'"],
-                   stderr=sys.stdout)
-    subprocess.run(["openssl", "x509", "-in", f"\'{CA_CERT}\'", "-purpose", "-noout", "-text"],
-                   stderr=sys.stdout)
-
-
 def generate_server_cert():
     subprocess.run(["openssl", "genrsa", "-out", f"\'{SERVER_CERT_KEY}\'", "4096"],
                    stderr=sys.stdout)
     subprocess.run(["openssl", "req", "-new", "-sha256", "-subj", "/CN=Jebbex", "-key", f"\'{SERVER_CERT_KEY}\'",
                     "-out", f"\'{SERVER_CSR}\'"],
                    stderr=sys.stdout)
-    open(EXTFILE, "w").write(EXTFILE_CONTENTS)
     subprocess.run(["openssl", "x509", "-req", "-sha256", "-days", "3650", "-in", f"\'{SERVER_CSR}\'", "-CA",
                     f"\'{CA_CERT}\'", "-CAkey", f"\'{CA_KEY}\'", "-out", f"\'{SERVER_CERT}\'", "-extfile",
                     f"\'{EXTFILE}\'", "-CAcreateserial"],
@@ -43,5 +22,4 @@ def generate_server_cert():
 
 
 if __name__ == '__main__':
-    generate_ca()
     generate_server_cert()
